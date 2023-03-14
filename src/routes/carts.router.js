5c79473e86a3889f97dbd67963c2ae7b02cc55c9
Marcelo Cardozo <marcelorcardozo@gmail.com>
2023-03-14 11:22:10 -0300

    Moving routers to new filesystem controllers

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,84 +1,14 @@
 import { response, Router } from "express";
-import CartManager from "../dao/Filesystem/managers/cartManager.js";
-import ProductManager from "../dao/Filesystem/managers/productManager.js";
+
+// Import controllers for Filesystem
+import { getCarts, getCartById, createCart, addProductToCart } from "../dao/Filesystem/controllers/cart.controller.js"
+
 
 const router = Router();
-let cartManager = new CartManager();
-let productManager = new ProductManager();
-cartManager.checkFileExist();
 
-router.get("/", async (request, response) => {
-  let limit = parseInt(request.query.limit);
-  let queryLimited = [];
-  console.log("Consumiendo api GET /api/carts...");
-  console.log("Esperando Carritos");
-  let cartsFile = await cartManager.readCarts();
-  if (limit !== undefined && limit <= cartsFile.length) {
-    for (let i = 0; i < limit; i++) {
-      queryLimited.push(cartsFile[i]);
-    }
-    cartsFile = queryLimited;
-  }
-  console.log(cartsFile);
-  response.send(JSON.stringify(cartsFile));
-});
-
-router.get("/:cid", async (request, response) => {
-  let cId = parseInt(request.params.cid);
-  let carts = await cartManager.readCarts();
-  const cartPosition = carts.findIndex((cart) => cart.id === cId);
-  console.log("Cart Position", cartPosition);
-  if (cartPosition < 0) {
-    return response
-      .status(404)
-      .send({ status: "info", message: "Carrito no encontrado" });
-  }
-  console.log("Carrito encontrado: ", carts[cartPosition]);
-  return response.send({
-    status: "Success",
-    message: "Carrito Encontrado.",
-    data: carts[cartPosition],
-  });
-});
-
-router.post("/", async (request, response) => {
-  let products = request.body;
-  let cartId = Math.floor(Math.random() * 100000 + 1);
-  await cartManager.addCart(cartId);
-  return response.send({
-    status: "Success",
-    message: "Carrito Creado.",
-    data: `Carrito creado con el ID: ${cartId}`,
-  });
-});
-
-router.post("/:cid/product/:pid", async (request, response) => {
-  let cid = parseInt(request.params.cid);
-  let pid = parseInt(request.params.pid);
-  let body = request.body;
-  let qty = parseInt(body["quantity"]);
-  let carts = await cartManager.readCarts();
-  let products = await productManager.returnProducts();
-  const productPosition = products.findIndex((product) => product.id === pid);
-  const cartPosition = carts.findIndex((cart) => cart.id === cid);
-  if (cartPosition < 0) {
-    return response
-      .status(404)
-      .send({ status: "info", message: "Carrito no encontrado" });
-  }
-  console.log(`Carrito: ${cid}, Producto: ${pid}, Cantidad: ${qty}`);
-  if (productPosition < 0) {
-    return response
-      .status(404)
-      .send({ status: "info", message: "Producto no valido" });
-  }
-  console.log(`Carrito: ${cid}, Producto: ${pid}, Cantidad: ${qty}`);
-  cartManager.updateCart(cid, pid, qty, cartPosition);
-  return response.send({
-    status: "Success",
-    message: "Carrito Actualizado.",
-    data: carts[cartPosition],
-  });
-});
+router.get("/", getCarts);
+router.get("/:cid", getCartById);
+router.post("/", createCart);
+router.post("/:cid/product/:pid", addProductToCart);
 
 export default router;
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,93 +1,18 @@
 import { Router } from "express";
-import ProductManager from "../modules/productManager.js";
+
+// Import Products controllers for Filesystem
+import {getProducts, getProductsById, createProduct, updateProductById, deleteProduct} from '../dao/Filesystem/controllers/products.controller.js'
 
 const router = Router();
-let productManager = new ProductManager();
-productManager.checkFileExist();
 
-router.get("/", async (request, response) => {
-  let limit = parseInt(request.query.limit);
-  let queryLimited = [];
-  console.log("Consumiendo api GET /api/products...");
-  console.log("Esperando Productos");
-  let productsFile = await productManager.returnProducts();
-  if (limit !== undefined && limit <= productsFile.length) {
-    for (let i = 0; i < limit; i++) {
-      queryLimited.push(productsFile[i]);
-    }
-    productsFile = queryLimited;
-  }
-  console.log(productsFile);
-  response.send(JSON.stringify(productsFile));
-});
+router.get("/", getProducts);
 
-router.get("/:pid", async (request, response) => {
-  let pId = parseInt(request.params.pid);
-  let product = await productManager.getProductById(pId);
-  if (product === null) {
-    return response
-      .status(404)
-      .send({ status: "info", message: "Producto no encontrado" });
-  }
-  console.log("Producto encontrado: ", product);
-  return response.send({
-    status: "Success",
-    message: "Producto Encontrado.",
-    data: product,
-  });
-});
+router.get("/:pid", getProductsById);
 
-router.post("/", async (request, response) => {
-  let { title, description, code, price, status, stock, category, thumbnails } =
-    request.body;
-  let productId = Math.floor(Math.random() * 100000 + 1);
-  await productManager.addProduct(
-    productId,
-    title,
-    description,
-    code,
-    price,
-    status,
-    stock,
-    category,
-    thumbnails
-  );
-  return response.send({
-    status: "Success",
-    message: "Producto Creado.",
-    data: `Producto creado con el ID: ${productId}`,
-  });
-});
+router.post("/", createProduct);
 
-router.put("/:pid", async (request, response) => {
-  let body = request.body;
-  let pId = parseInt(request.params.pid);
-  let product = await productManager.getProductById(pId);
-  if (product === null) {
-    return response
-      .status(404)
-      .send({ status: "info", message: "Producto no encontrado" });
-  }
-  await productManager.updateProduct(pId, body);
-  return response.send({
-    status: "Success",
-    message: "Producto Actualizado.",
-  });
-});
+router.put("/:pid", updateProductById);
 
-router.delete("/:pid", async (request, response) => {
-  let pId = parseInt(request.params.pid);
-  let product = await productManager.getProductById(pId);
-  if (product === null) {
-    return response
-      .status(404)
-      .send({ status: "info", message: "Producto no encontrado" });
-  }
-  await productManager.deleteProduct(pId);
-  return response.send({
-    status: "Success",
-    message: "Producto Borrado.",
-  });
-});
+router.delete("/:pid", deleteProduct);
 
 export default router;
